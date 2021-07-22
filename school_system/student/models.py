24f85c43b8fc78cd6c5d45c0d78aec67c7c991cf
chantal2000 <nniyonkuruchantal@gmail.com>
2021-07-22 22:13:56 +0300

    Student Atribute

--- school_system/student/models.py
+++ school_system/student/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from django.db.models.fields import PositiveBigIntegerField
 
 # Create your models here.
@@ -7,3 +8,27 @@ class Student(models.Model):
     last_name=models.CharField(max_length=12)
     age=models.PositiveSmallIntegerField()
     date_of_birth=models.DateField()
+    Age=models.PositiveSmallIntegerField()
+    Roll_number=models.CharField(max_length=5)
+    student_id=models.PositiveSmallIntegerField()
+    National_Id=models.CharField(max_length=16)
+    CHOICES=(
+        ('F',"Female"),
+        ('M',"Male")
+    )
+    Gender=models.CharField(max_length=10,choices=CHOICES)
+    Guardian_name=models.CharField(max_length=40)
+    Email_address=models.EmailField(max_length=30)
+    Country=models.CharField(max_length=30)
+    profile_image=models.CharField(max_length=20)
+    Grade=models.CharField(max_length=2)
+    Medical_report=models.FileField(upload_to='uploads',blank=True)
+    date_Of_enrollment=models.DateField()
+    course_name=models.CharField(max_length=30)
+    Laptop_number=models.CharField(max_length=7)
+    # Languages=models.ListField()
+    # phone_number=models.Phone
+
+
+    
+   
--- school_system/student/views.py
+++ school_system/student/views.py
@@ -1,3 +1,10 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from .forms import StudentRegistrationForm
 from django.shortcuts import render
 
 # Create your views here.
+def register_student(request):
+    form=StudentRegistrationForm()
+    return render(request,"register_student.htm",{"form":form})
+
